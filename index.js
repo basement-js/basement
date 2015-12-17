7e763949d0f90e98aed11bb055b3ce0aa133fc90
Ruben Rutten <ruben@rubenrutten.nl>
2015-12-17 23:49:51 +0100

    - Added `socket.io`, `bunyan` for logging and `restify` - Removed `cookie`, `cookie-parser`, `body-parser`, `express` and `winston`

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 "use strict";
 
-modules.exports = exports = app;
+var restify = require("restify");
+var socketio = require("socket.io");
+var bunyan = require("bunyan");
+
+
+var server = restify.createServer();
+var io = socketio.listen(server);
+
+server.on('after', restify.auditLogger({
+	log: bunyan.createLogger({
+		name: 'audit',
+		stream: process.stdout
+	})
+}));
+
+server.listen(process.env.PORT || 3000, function() {
+	console.log("Server running at %s", server.url);
+});
+
+module.exports = exports = server;
