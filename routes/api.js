8dc47f1baed3c6aa95b07ee4e383b2ec5ecc6547
Alexander Sagen <alexrsagen@gmail.com>
2015-12-21 22:46:18 +0100

    Update file structure of the API
    This will allow for cleaner code, and make API endpoints easier to separate.

--- routes/api.js
+++
@@ -1,9 +0,0 @@
-// "server" is a restify instance
-module.exports = function (vars) {
-	// shorthand for vars.server
-	var server = vars.server;
-
-	// server.get("/api/hello/:name", function (req, res, next) {
-	// 	res.send("hello " + req.params.name)
-	// });
-}
