6adc2ebf2fb7bb64ad011efe3d5e29b9fe180c6e
Alexander Sagen <alexrsagen@gmail.com>
2016-01-02 17:34:06 +0100

    Add collection example

---
+++ bookshelf/collections/example.js
@@ -0,0 +1,10 @@
+// Remove this file once a real collection is added
+// 
+// module.exports = function () {
+//     var bookshelf = this.bookshelf;
+//     var TestCollection = bookshelf.Collection.extend({
+//         model: bookshelf.model("TestTable")
+//     });
+
+//     return this.bookshelf.collection("TestCollection", TestCollection);
+// }
